21efdf4451f629771d3f8f95f72e5371896bf016
Alex De Simone <ajdesimone@gmail.com>
2012-08-22 00:24:19 -0700

    include begin/rescue in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,12 +16,18 @@ class SessionsController < ApplicationController
     user = nil
     client = nil 
     session[:user_id] = nil
-    # client.materialize(nil)
+    begin
+      client.materialize(nil)
+    rescue
+    end
     redirect_to root_url, :notice => "Signed out!"
   end
 end
 
 
-# client = Databasedotcom::Client.new :client_id => "3MVG9y6x0357HledZQgj1XEr5IilzYK68ewOkmN9bgVxZtyMlDY8_QqVB_Fg9GqK7.SQjoXD5fsBqF49nDHLR", :client_secret => "3725793166232094938", :user_id => "005E0000000epp7IAA"
-# client.authenticate :token => "00DE0000000ZfeY!AQwAQA_8ttEavjfejJxKDzC5UGWFQrV0NXXPysRAONeZUH_wBtijVePXseieqSQQ5eNzS6FbSESUC8GQln7avuOsq7a3ourF", :instance_url => "https://na9.salesforce.com", :refresh_token => "5Aep861rEpScxnNE66Rv3QieBZJ8TZrgI70w4cSOl2rhcUsmXqDGdthH98V3ff4aCek4TBRmYMJPQ=="
+# client = Databasedotcom::Client.new :client_id => "3MVG9y6x0357HledZQgj1XEr5IilzYK68ewOkmN9bgVxZtyMlDY8_QqVB_Fg9GqK7.SQjoXD5fsBqF49nDHLR", :client_secret => "3725793166232094938", :user_id => "005E0000000GmckIAC", :sobject_module => "Module"
+# client.authenticate :token => "00DE0000000aHNC!AQIAQPKeQ8Euq06X8W02jf5WbhzCKgp0D.to8D5HEWebOzi9jxnTCcsx00rVWAduE7_EuXmGhgebRxHJwX.OLKNcVBWKEtSt", :instance_url => "https://na9.salesforce.com", :refresh_token => "5Aep861rEpScxnNE64..BjXbfMFaOoZVy8dErI3z9N4SxAtcTns4NPlM46qnrvFlGbbcJJjichuBw=="
+
+# client = Databasedotcom::Client.new :client_id => "3MVG9y6x0357HledZQgj1XEr5IilzYK68ewOkmN9bgVxZtyMlDY8_QqVB_Fg9GqK7.SQjoXD5fsBqF49nDHLR", :client_secret => "3725793166232094938", :user_id => "005E0000000epp7IAA", :sobject_module => "Module"
+# client.authenticate :token => "00DE0000000ZfeY!AQwAQDHh72orVdH_vVwqGq7t6eYr3TcPjms3boUzpTRJy1mAFKoZxD31n19QUMAVhc_TEYtNG5qzL1wwcax3a95p9pr0wi_c", :instance_url => "https://na9.salesforce.com", :refresh_token => "5Aep861rEpScxnNE66Rv3QieBZJ8TZrgI70w4cSOl2rhcUsmXpZCk3mFcE6DbXzt99Mmb4aL8Of5g=="
 

8a0c300eb13117026885ec907dcab4e769615332
Alex De Simone <ajdesimone@gmail.com>
2012-08-21 22:33:12 -0700

    Included owner_id in client in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to '/leads/new', :notice => "Signed in!"
-    client = Databasedotcom::Client.new
+    client = Databasedotcom::Client.new :client_id => "3MVG9y6x0357HledZQgj1XEr5IilzYK68ewOkmN9bgVxZtyMlDY8_QqVB_Fg9GqK7.SQjoXD5fsBqF49nDHLR", :client_secret => "3725793166232094938", :user_id => user.owner_id
     client.authenticate :token => user.token, :instance_url => user.instance_url, :refresh_token => user.refresh_token
     client.materialize("Lead")
   end
@@ -19,3 +19,8 @@ class SessionsController < ApplicationController
     redirect_to root_url, :notice => "Signed out!"
   end
 end
+
+
+# client = Databasedotcom::Client.new :client_id => "3MVG9y6x0357HledZQgj1XEr5IilzYK68ewOkmN9bgVxZtyMlDY8_QqVB_Fg9GqK7.SQjoXD5fsBqF49nDHLR", :client_secret => "3725793166232094938", :user_id => "005E0000000epp7IAA"
+# client.authenticate :token => "00DE0000000ZfeY!AQwAQA_8ttEavjfejJxKDzC5UGWFQrV0NXXPysRAONeZUH_wBtijVePXseieqSQQ5eNzS6FbSESUC8GQln7avuOsq7a3ourF", :instance_url => "https://na9.salesforce.com", :refresh_token => "5Aep861rEpScxnNE66Rv3QieBZJ8TZrgI70w4cSOl2rhcUsmXqDGdthH98V3ff4aCek4TBRmYMJPQ=="
+

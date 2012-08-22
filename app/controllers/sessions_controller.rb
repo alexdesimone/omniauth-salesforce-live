ee3f73016a552f44c73320989e1acf45293d4af9
Alex De Simone <ajdesimone@gmail.com>
2012-08-21 22:44:43 -0700

    Add client.materialize(nil) as test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
     user = nil
     client = nil 
     session[:user_id] = nil
+    client.materialize(nil)
     redirect_to root_url, :notice => "Signed out!"
   end
 end
